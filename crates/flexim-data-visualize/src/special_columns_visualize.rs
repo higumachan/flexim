44b85e1905945d45690c9163a46fc07e650140b0
Yuta Hinokuma <higumachan@users.noreply.github.com>
2024-02-27 23:14:04 +0900

    Segmentに対してLabelを描画できるようにする

--- crates/flexim-data-visualize/src/special_columns_visualize.rs
+++ crates/flexim-data-visualize/src/special_columns_visualize.rs
@@ -114,6 +114,7 @@ impl SpecialColumnShape for FlDataFrameSegment {
         let RenderParameter {
             stroke_color: color,
             stroke_thickness: thickness,
+            label,
             ..
         } = parameter;
 
@@ -123,6 +124,7 @@ impl SpecialColumnShape for FlDataFrameSegment {
         let segment_p2 = Pos2::new(self.x2 as f32, self.y2 as f32) * state.scale
             + state.shift
             + painter.clip_rect().min.to_vec2();
+        let center = (segment_p1 + segment_p2.to_vec2()) / 2.0;
 
         let rectangle = Rect::from_min_max(segment_p1, segment_p2);
         let response = if rectangle.width() < 1.0 || rectangle.height() < 1.0 {
@@ -130,8 +132,7 @@ impl SpecialColumnShape for FlDataFrameSegment {
         } else {
             let p1_rectangle = Rect::from_center_size(segment_p1, Vec2::splat(1.0));
             let p2_rectangle = Rect::from_center_size(segment_p2, Vec2::splat(1.0));
-            let center_rectangle =
-                Rect::from_center_size((segment_p1 + segment_p2.to_vec2()) / 2.0, Vec2::splat(1.0));
+            let center_rectangle = Rect::from_center_size(center, Vec2::splat(1.0));
 
             ui.allocate_rect(p1_rectangle, Sense::click())
                 .union(ui.allocate_rect(p2_rectangle, Sense::click()))
@@ -139,6 +140,27 @@ impl SpecialColumnShape for FlDataFrameSegment {
         };
 
         painter.line_segment([segment_p1, segment_p2], Stroke::new(thickness, color));
+
+        let response = if let Some(label) = label {
+            let text_rect = painter.text(
+                center,
+                Align2::CENTER_CENTER,
+                label.as_str(),
+                FontId::default(),
+                Color32::BLACK,
+            );
+            painter.rect_filled(text_rect, 0.0, color);
+            let text_rect = painter.text(
+                center,
+                Align2::CENTER_CENTER,
+                label.as_str(),
+                FontId::default(),
+                Color32::BLACK,
+            );
+            response | (ui.allocate_rect(text_rect, Sense::click()))
+        } else {
+            response
+        };
 
         Some(response)
     }
