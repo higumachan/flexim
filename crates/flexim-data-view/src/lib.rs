44eb927ec7fd661542650d95de3003eccb9285cc
Yuta Hinokuma <higumachan@users.noreply.github.com>
2024-05-08 00:27:24 +0900

    列の表示・非表示の切り替えが出来るようにする (#134)
    * 列の表示・非表示の切り替えが出来るようにする
    
    * fix

--- crates/flexim-data-view/src/lib.rs
+++ crates/flexim-data-view/src/lib.rs
@@ -1,5 +1,8 @@
+use egui::ahash::HashMap;
 use flexim_data_type::{FlData, FlDataReference};
-use flexim_table_widget::FlTable;
+use flexim_table_widget::{FlTable, FlTableDrawContext};
+use itertools::Itertools;
+use std::sync::{Arc, Mutex};
 
 use rand::random;
 use serde::{Deserialize, Serialize};
@@ -10,6 +13,36 @@ pub type Id = u64;
 pub struct FlDataFrameView {
     pub id: Id,
     pub table: FlTable,
+    pub view_context: Arc<Mutex<FlDataFrameViewContext>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum ShowColumns {
+    All,
+    Some(HashMap<String, usize>),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FlDataFrameViewContext {
+    pub show_columns: ShowColumns,
+}
+
+impl From<FlDataFrameViewContext> for FlTableDrawContext {
+    fn from(view: FlDataFrameViewContext) -> Self {
+        let draw_columns = match view.show_columns {
+            ShowColumns::All => flexim_table_widget::ShowColumns::All,
+            ShowColumns::Some(columns) => {
+                let mut columns = columns.iter().map(|(k, v)| (k.clone(), *v)).collect_vec();
+                columns.sort_by_key(|(_, v)| *v);
+                let columns = columns.into_iter().map(|(k, _)| k).collect();
+                flexim_table_widget::ShowColumns::Some(columns)
+            }
+        };
+
+        Self {
+            show_columns: draw_columns,
+        }
+    }
 }
 
 impl FlDataFrameView {
@@ -17,6 +50,9 @@ impl FlDataFrameView {
         Self {
             id: gen_id(),
             table: FlTable::new(data_reference),
+            view_context: Arc::new(Mutex::new(FlDataFrameViewContext {
+                show_columns: ShowColumns::All,
+            })),
         }
     }
 }
